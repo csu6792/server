27700a6998a21615d4e80c8b18b6bc2d678cf4bd
k40118132 <53500308+k40118132@users.noreply.github.com>
2021-05-31 15:21:42 +0800

    Add files via upload

--- rpiserver.py
+++ rpiserver.py
@@ -1,41 +1,28 @@
 import socket
 import threading
 
-
 class Streamer(threading.Thread):
-
     def __init__(self, hostname, port):
         threading.Thread.__init__(self)
-
         self.hostname = hostname
         self.port = port
         self.running = False
         self.streaming = False
         #self.s = s
         #self.payload_size = payload_size
-
     def run(self):
-
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         print('Socket created')
-
         self.s.bind((self.hostname, self.port))
         print('Socket bind complete')
-
         #self.payload_size = struct.calcsize("Q")
-
         self.s.listen(10)
         print('Socket now listening')
-
         self.running = True
-
         while self.running:
-
             print('Start listening for connections...')
-
             self.conn, addr = self.s.accept()
             print("New connection accepted.")
-
             while True:
                 data = self.conn.recv(1024)
                 #print(data.decode())
@@ -45,10 +32,8 @@ class Streamer(threading.Thread):
                 else:
                     break
         print('Exit thread.')
-
     def stop(self):
         self.running = False
-
     def sendmessage(self,message):
         outdata = str(message)
         self.conn.sendall(outdata.encode())
